8f8ef0578c4111d85dfd9751de0dd774b04838e9
BCsabaEngine <csaba@balazs-magyar.hu>
2026-01-16 08:43:40 +0100

    feat: demo fix

--- demo/tailwind.config.cjs
+++ demo/tailwind.config.cjs
@@ -1,17 +1,13 @@
 const config = {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
 	],
-
-	plugins: [require('flowbite/plugin')],
 
 	darkMode: 'class',
 
 	theme: {
 		extend: {
 			colors: {
-				// flowbite-svelte
 				primary: {
 					50: '#FFF5F2',
 					100: '#FFF1EE',
